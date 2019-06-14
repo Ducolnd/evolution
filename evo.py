0477569babce449a321fd5856b12f48684a10ed9
Duco Lindhout <duco.lindhout@gmail.com>
2019-06-14 17:45:56 +0200

    Made array for map

--- evo.py
+++ evo.py
@@ -1,6 +1,25 @@
 #Importing modules
-import numpy as np
 import random
-import numpy
+
+#Game variables
+x = 10
+y = 5
+a = 5
+gameMap = [[[0 for i in range(a)] for b in range(x)] for u in range(y)]
+gameMap[2][9] = [236, 754, 346, 663, 266]
+print(gameMap)
+
+
+#Generation variables
+speed = 0
+size = 0
+fdr = 0 #FoodDetectionRange
+edr = 0 #EnemyDetectionRange
+mos = 0 #MatingOffSet
+
+
+
+
+
 
 
