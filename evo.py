11241671abf29dc1c37ee41e20498d9ee569e69c
Duco Lindhout <duco.lindhout@gmail.com>
2019-06-15 09:45:40 +0200

    Added random player deployment

--- evo.py
+++ evo.py
@@ -1,14 +1,17 @@
 #Importing modules
 import random
+import numpy as np
+from pprint import pprint
 
 #Game variables
 x = 10
 y = 5
 a = 5
-gameMap = [[[0 for i in range(a)] for b in range(x)] for u in range(y)]
-gameMap[2][9] = [236, 754, 346, 663, 266]
-print(gameMap)
+players = 10
+gen = []
 
+gameMap = [[[0 for i in range(a)] for b in range(x)] for u in range(y)]
+pprint(gameMap)
 
 #Generation variables
 speed = 0
@@ -17,9 +20,18 @@ fdr = 0 #FoodDetectionRange
 edr = 0 #EnemyDetectionRange
 mos = 0 #MatingOffSet
 
+#Functions
+def checkSurrounding(i, j, x, y):
+	pass
 
 
+#Place players on random locations
+for i in range(players):
+	gen = []
+	for i in range(a): #Create random generation
+		gen.append(random.randint(1, 1000))
+	gameMap[random.randint(0, y-1)][random.randint(0, x-1)] = gen
 
-
+pprint(gameMap)
 
 
