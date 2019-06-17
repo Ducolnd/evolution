8a0ecfbba729e8542ea994a900608449badbf227
Duco Lindhout <duco.lindhout@gmail.com>
2019-06-17 18:47:36 +0200

    Instead of function based game, class based game

--- evo.py
+++ evo.py
@@ -1,47 +1,29 @@
 #Importing modules
 import random
-import numpy as np
-from pprint import pprint
 import pygame
-import string
 
 pygame.init()
 
 #Game variables
-margin = 1
 WIDTH = 15 #10 perfect with x = 90
 HEIGHT = WIDTH
-x = 40 #Most perfect is 90
-y = x
-a = 5
-players = 10
-gen = []
+tilesx = 40 #Most perfect is 90
+tilesy = tilesx
+
+starting = 100
 fsr = 200 #FoodSpawnRate per fsr iterations 
 fc = 0 #FoodCounter to track when to spawn
 wc = 0 #WalkingCounter to track when to move player
-dirs = []
 current = []
 
-gameMap = [[0 for b in range(x)] for u in range(y)]
-
-#Generation variables
-speed = 0
-size = 0
-fdr = 0 #FoodDetectionRange
-edr = 0 #EnemyDetectionRange
-mos = 0 #MatingOffSet
-
-#Player variables
-ttns = 0
-food = 0
+gameMap = [[0 for b in range(tilesx)] for u in range(tilesy)]
 
 #Pygame
-screen_height = y*HEIGHT+y*margin+1
-screen_width = x*WIDTH+x*margin+1
-
+screen_height = tilesy*HEIGHT+tilesy*margin+1
+screen_width = tilesx*WIDTH+tilesx*margin+1
 win = pygame.display.set_mode((screen_height, screen_width))
 pygame.display.set_caption("Evolution")
-done = False
+run = True
 clock = pygame.time.Clock()
 
 BLACK = (0, 0, 0)
@@ -50,99 +32,71 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
 
-#Functions
+class Players(object):
+	def __init__(self, speed, size, fdr, edr, mos, x, y):
+		#Generation variables
+		self.speed = speed
+		self.size = size
+		self.fdr = fdr #FoodDetectionRange
+		self.edr = edr #EnemyDetectionRange
+		self.mos = mos #MatingOffSet
+		self.x = x
+		self.y = y
 
-def spawnFood():
-	rx = random.randint(0, x-1)
-	ry = random.randint(0, y-1)
-	while gameMap[ry][rx] is not 0:
-		rx = random.randint(0, x-1)
-		ry = random.randint(0, y-1)
-	gameMap[ry][rx] = "food"
+		#Player variables
+		self.ttnm = 0
+		self.food = 0
+		self.color = [(self.speed + self.size)*255/200, (self.fdr + self.edr)*255/200, self.mos*255/100]
 
-def checkSurroundings():
-	pass
+	def move(self, fx, fy):
+		if fx < 0:
+			fx = 0
+		if fy < 0:
+			fy = 0
+		if fx > tilesx-1:
+			fx = tilesx
 
-def move(where, place, who):
-	gameMap[where[0]][where[1]] = who
-	gameMap[place[0]][place[1]] = 0
+		gameMap[fy][fx] = [self.speed, self.size, self.fdr, self.edr, self.mos]
+		gameMap[self.y][self.x] = 0
+		self.x = fx
+		self.y = fy
 
-def birth():
-	pass
-
+	def draw(self, win):
+		pygame.draw.rect(win, self.color, ((margin+WIDTH)*(self.x)+margin, (margin+HEIGHT)*(self.y)+margin, WIDTH, HEIGHT))
 
 #Place x players on random locations
-for i in range(players):
-	rx = random.randint(0, x-1)
-	ry = random.randint(0, y-1)
-	location = [ry, rx]
-	gen = []
-	for i in range(a):
-		gen.append(random.randint(1, 100))
-	gen.extend([ttns, random.randint(0,20), location])
+for i in range(starting):
+	speed = random.randint(1,100)
+	size = random.randint(1,100)
+	fdr = random.randint(1,100)
+	edr = random.randint(1,100)
+	mos = random.randint(1,100)
+	x = random.randint(1, tilesx-1)
+	y = random.randint(1, tilesy-1)
 
-	current.append(gen)
+	current.append(Players(speed, size, fdr, edr, mos, x, y))
+	gameMap[y][x] = 
 
-	while gameMap[ry][rx] is not 0:
-		rx = random.randint(0, x-1)
-		ry = random.randint(0, y-1)
+#Functions
+def function():
+	pass
 
-	gameMap[ry][rx] = gen
-
-
-while not done:
-	fc += 1
+while run:
+	wc += 1
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			done = True
+			run = False
+	win.fill(WHITE)
 
-	win.fill(BLACK)
-
-	if fc > fsr:
-		spawnFood()
-
-	for i in current:
-		if i[6] < 1:
-			gameMap[i[7][0]][i[7][1]] = 0
-			current.remove(i)
-			continue
-		i[5] += 1
-		if i[5] > i[0]:
-			i[6] += -1
-			i[5] = 0
-			rx = random.randint(-1,1)
-			if rx == 0:
-				ry = random.choice([-1,1])
-			else:
-				ry = random.randint(-1,1)
-			ry = i[7][0]-ry
-			rx = i[7][1]-rx
-
-			if ry < 0:
-				ry = 0
-			if ry > x:
-				ry = (x-1)
-			if rx < 0:
-				rx = 0
-			if rx > x:
-				rx = (x-1)
-			if gameMap[ry][rx] == "food":
-				i[6] += 10
-			loc = [ry, rx]
-			move(loc, i[7], i[0:5])
-			i[7] = loc
+	for player in current:
+		player.draw(win)
 
 
-	for row in range(y):
-		for column in range(x):
-			if gameMap[row][column] == 0:
-				color = WHITE
-			elif gameMap[row][column] == "food":
-				color = RED
-			else:
-				gen = gameMap[row][column]
-				color = ((gen[2]*255/100), (gen[2]*255/100), (((gen[2]+gen[3]+gen[4] )/3)*255/100) )
-			pygame.draw.rect(win, color, ((margin+WIDTH)*column+margin, (margin+HEIGHT)*row+margin, WIDTH, HEIGHT))
+	if wc > 5:
+		wc = 0
+		for player in current:
+			player.move(player.x, player.y-1)
+
 
 	clock.tick(60)
 	pygame.display.flip()
