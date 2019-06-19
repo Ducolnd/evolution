39df5b5ece2a88645a0bf0a4999afda6b1e55ff6
JelmerLnd <jelmer.lindhout@gmail.com>
2019-06-19 17:40:40 +0200

    added info panel

--- evo.py
+++ evo.py
@@ -11,6 +11,7 @@ WIDTH = 20 #10 perfect with x = 90
 HEIGHT = WIDTH
 tilesx = 40 #Most perfect is 90
 tilesy = tilesx
+information_panel_width = 200
 
 starting = 10
 fsr = 60 #FoodSpawnRate per fsr iterations 
@@ -86,21 +87,23 @@ class Players(object):
 
 	def die(self):
 		current.remove(self)
+		gameMap[y][x] = 0
 		gameMap[self.y][self.x] = 0
 
+
         def calc_move(self):
-	
+
+                
 	def find_objective(self, objective):
                 
 		
 
 	def wander(self):
 		ry = random.randint(-1, 1)
-		if not ry: #If ry = 0 so it will not not move.
+		if not ry: #If 
 			rx = random.choice([-1, 1])
 		else:
 			rx = random.randint(-1, 1)
-		self.move(self.x+rx, self.y+ry)
 		
                         
 class Food(object):
@@ -142,6 +145,9 @@ def spawnPlayer():
 	else:
 		spawnPlayer()
 
+def Information_panel_draw():
+        
+
 #Place x players on random locations
 for i in range(starting):
 	spawnPlayer()
@@ -167,7 +173,7 @@ while run:
 		player.draw(win)
 	for snacks in currentFood: #Draw all objects in class Players
 		snacks.draw(win)
-
+	Information_panel_draw()
 	clock.tick(60)
 	pygame.display.flip()
 
